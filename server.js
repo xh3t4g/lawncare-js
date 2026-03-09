44f29e4c3ed86cd8b66b7bc112928ccd32694358
xh3t4g <xh3t4g@gmail.com>
2026-03-09 20:32:05 +0300

    добавил server.js -> express.js

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(3000, () => {
+    console.log("Успешный запуск")
+})
